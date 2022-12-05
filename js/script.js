518e0a6b9080dda32684142dfaa9ba3ead4ab389
HieuNguyenIT5 <hieukhac6869@gmail.com>
2022-12-05 14:05:58 +0700

    code banner, listing, testimonials, brand

--- js/script.js
+++ js/script.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
     $(".slider-content").slick({
-        dots: true,
+        autoplay:true,
+        autoplaySpeed:2000,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
         nextArrow: '<i class="next-slider fa-solid fa-chevron-left"></i>',
         prevArrow: '<i class="prev-slider fa-solid fa-chevron-right"></i>'
+    })
+    $(".testimonials").slick({
+        // autoplay:true,
+        // autoplaySpeed:2000,
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
     })
 });
