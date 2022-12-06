482a5b9c4b5a54a9cbb8719c211c13f2c8dc611d
HieuNguyenIT5 <hieukhac6869@gmail.com>
2022-12-06 19:12:58 +0700

    Finishing bottom

--- js/script.js
+++ js/script.js
@@ -9,11 +9,30 @@ $(document).ready(function () {
         prevArrow: '<i class="prev-slider fa-solid fa-chevron-right"></i>'
     })
     $(".testimonials").slick({
-        // autoplay:true,
-        // autoplaySpeed:2000,
+        autoplay:true,
+        autoplaySpeed:2000,
         dots: true,
         infinite: true,
         speed: 300,
         slidesToShow: 1,
     })
+    $("#tab-bar-listing .btn-tab-bar").click(function(){
+        $("#tab-bar-listing .btn-tab-bar.selected").removeClass("selected")
+        $(this).addClass("selected")
+        var active = $("#tab-bar-listing .btn-tab-bar.selected").attr('data')
+        switch(active){
+            case '1':
+                $(".listing .active").removeClass('active')
+                $(".listing .top-sellers").addClass('active')
+                break;
+            case '2':
+                $(".listing .active").removeClass('active')
+                $(".listing .featured").addClass('active')
+                break;
+            case '3':
+                $(".listing .active").removeClass('active')
+                $(".listing .most-reviews").addClass('active')
+                break;
+        }
+    });
 });
